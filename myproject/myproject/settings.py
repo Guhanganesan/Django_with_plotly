904deeb4c8fe0bd5f095d3ea14b18c4e78ef0545
Guhan Ganesan <phpguhan@gmail.com>
2023-02-14 22:46:36 +0530

    steps to run django app

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'myproject.urls'
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('django_plotly_dash/', include('django_plotly_dash.urls'))
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('', include('myapp.urls'))
 ]
