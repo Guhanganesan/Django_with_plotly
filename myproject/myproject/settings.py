fb311882f7c3e621c3085341fe83f2c129315aec
Guhan Ganesan <phpguhan@gmail.com>
2023-04-02 23:50:36 +0530

    update postgresql credentials

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'Employee',  
+        'USER':'postgres',  
+        'PASSWORD':'1234',  
+        'HOST':'localhost',  
+        'PORT':'5432'  
     }
 }
 
